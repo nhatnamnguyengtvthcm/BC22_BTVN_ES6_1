b73157669b40315ed36f92f125dc7fd81b92fd67
Namnn <nhatnam.nguyen@masterisehomes.com>
2022-05-01 17:46:58 +0700

    up

--- baitapArrowFunc/js/main.js
+++ baitapArrowFunc/js/main.js
@@ -1,19 +1,28 @@
 const colorList = ["pallet", "viridian", "pewter", "cerulean", "vermilion", "lavender", "celadon", "saffron", "fuschia", "cinnabar"];
 
+window.changeColor = (color)=>{
+    console.log(color);
+    const houseEle = document.getElementById("house");
+    houseEle.className = "house " + color;
+
+}
+
 const loadColorList = () => {
     const colorListHtml =  colorList.reduce((result, color , index) =>{
         if(index == 0){
             return result += `
-                <button class="color-button ${color} active"></button> 
+                <button class="color-button ${color} active" onclick="changeColor('${color}')"></button> 
             `
         }
         else {
             return result += `
-            <button class="color-button ${color}"></button> 
+                <button class="color-button ${color}"  onclick="changeColor('${color}')"></button> 
             `
         }
-    }, "")
-    console.log(colorListHtml);
+    }, "");
+    // console.log(colorListHtml);
     document.getElementById("colorContainer").innerHTML = colorListHtml;
 };
 loadColorList();
+// changeColor();
+
